5f21f3033bdda3f283b22e13f66d8a4511dcc149
Bassey John <jsbazman@gmail.com>
2024-02-15 11:09:44 +0100

    added postgres configuration

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'income_expense',
+        'USER': 'postgres',
+        'PASSWORD': '9507024922',
+        'HOST':'localhost'
+    }   
 }
 
 

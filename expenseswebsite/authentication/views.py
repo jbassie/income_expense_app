55787e7ea873bad9a6256cacfdc680f73f3ee650
Bassey John <jsbazman@gmail.com>
2024-03-28 15:37:10 +0100

    setup messages

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -4,12 +4,22 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 
 # Create your views here.
 
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+    def post(self, request):
+        messages.success(request, "Success")
+        messages.warning(request, "Warning")
+        messages.info(request, "Info")
+        messages.error(request, "Error")
+        return render(request, 'authentication/register.html')
+
+
 
 
 class UsernameValidationView(View):

d8bf713e1d408260a10b2e619be29d36ff30cf11
jbassie <jsbazman@gmail.com>
2024-03-29 11:24:05 +0100

    Issue 7

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,6 +1,7 @@
-from .views import RegistrationView
+from .views import RegistrationView, UserNameValidationView
 from django.urls import path
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name="register"),
+    path('validate-username', UserNameValidationView.as_view())
 ]
--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+import json
 # Create your views here.
+
+class UserNameValidationView(View):
+    def post(self, request):
+        data = json.leads(request.body)
+        username=data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric character'})
+        
+        return JsonResponse({'username_valid', True})
 
 class RegistrationView(View):
     def get(self, request):

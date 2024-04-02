f5d2594736462f325fa340cca3a5af4cb254e213
Bassey John <jsbazman@gmail.com>
2024-04-02 17:42:37 +0100

    CRUD implemented on Expenses

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
+     path('expense_edit/<int:id>', views.expense_edit, name="expense_edit"),
+     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
     path('', views.index, name="expenses"),
-    path('add-expense', views.add_expense, name='add-expenses')
-
+    path('add-expense/', views.add_expense, name='add-expenses'),
+   
 ]

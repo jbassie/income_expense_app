edd37621d0130dbab1066f5bb9e2fd831e14f723
Bassey John <jsbazman@gmail.com>
2024-08-05 07:30:29 +0100

    formatted urls

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('register', RegistrationView.as_view(), name="register"),
-    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
-    path('validate-email',csrf_exempt(EmailValidationView.as_view()), name='validate-email' ),
+    path('register/', RegistrationView.as_view(), name="register"),
+    path('validate-username/', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
+    path('validate-email/',csrf_exempt(EmailValidationView.as_view()), name='validate-email' ),
     #path('activate/<uidb64>/<token>', VerificationView.as_view(), name="activate")
-    path('login', LoginView.as_view(), name="login"),
-    path('logout', LogOutView.as_view(), name="logout")
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogOutView.as_view(), name="logout")
 ]
